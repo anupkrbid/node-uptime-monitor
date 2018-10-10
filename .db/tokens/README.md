5fbf8a7ceba443f0f3f3c08a8d35b0062eb5d77d
anupkrbid <anupbid94@gmail.com>
2018-10-11 00:33:15 +0530

    Service 3: /tokens

---
+++ .db/tokens/README.md
@@ -0,0 +1,5 @@
+## .db/tokens
+
+# Sudo Database -> Tokens Table
+
+This folder stored all data related to user tokens for this application
--- server.js
+++ server.js
@@ -112,5 +112,6 @@ const unifiedServer = (req, res) => {
 const router = {
   ping: handlers.ping,
   users: handlers.users,
+  tokens: handlers.tokens,
   hello: handlers.hello
 };
